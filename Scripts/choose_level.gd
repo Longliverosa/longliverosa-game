b89b53a5c2adf003fcd61c733925914d78df279d
Loikas <50876764+Loikas@users.noreply.github.com>
2025-08-14 00:03:33 +0200

    Improved the level editor to work better and be usable for testing (#35)
    Co-authored-by: Lucas <Lucas@DESKTOP-95H870K>

--- Scripts/choose_level.gd
+++ Scripts/choose_level.gd
@@ -64,3 +64,7 @@ func _on_next_level_pressed() -> void:
 	previous_button.visible = current_level_index != 0
 	next_button.visible = current_level_index != levels.size() - 1 
 	set_information_for_current_level()
+
+
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Menu/main_menu.tscn")
--- Scripts/level_editor.gd
+++ Scripts/level_editor.gd
@@ -1,4 +1,5 @@
 extends Control
+class_name LevelEditor
 
 @onready var tile_map : TileMapLayer = %TileMap
 @onready var item_select : ItemList = %ItemSelect
@@ -16,7 +17,10 @@ var tiles : Array[String]
 var entities : Array[EntityBase]
 var current_tile_index : int = Categories.TILES
 var current_category : int = Categories.TILES
- 
+
+var is_player_placed : bool = false
+var created_should_check_for_load = true
+
 func _ready() -> void:
 	for i in range(tile_map.tile_set.get_terrains_count(0)):
 		tiles.append(tile_map.tile_set.get_terrain_name(0, i))
@@ -29,6 +33,17 @@ func _ready() -> void:
 			
 	tile_map.set_owner(created_scene)
 
+func reset_created_scene_vars() -> void:
+	created_scene = get_node("CreatedScene")
+	tile_map = created_scene.get_node("TileMap")
+	for node in created_scene.get_children():
+		if node is EntitySpawn and node.entity.name == "Player":
+			for i in range(item_select.item_count):
+				if entities[i].name == "Player":
+					is_player_placed = true
+					break
+	set_item_select_for_category(Categories.TILES)
+
 func _on_category_select_item_selected(index: int) -> void:
 	set_item_select_for_category(index)
 
@@ -38,22 +53,32 @@ func set_item_select_for_category(category : int) -> void:
 	if category == Categories.TILES:
 		for tile in tiles:
 			item_select.add_item(tile)
+		for item in created_scene.get_children():
+			if item is EntitySpawn:
+				item.mouse_filter = Control.MOUSE_FILTER_IGNORE
 	elif category == Categories.ENTITIES:
 		for entity in entities:
-			item_select.add_item(entity.name, entity.icon)	
+			var index = item_select.add_item(entity.name, entity.icon)
+			if entity.name == "Player" and is_player_placed:
+				item_select.set_item_disabled(index, true)
+		for item in created_scene.get_children():
+			if item is EntitySpawn:
+				item.mouse_filter = Control.MOUSE_FILTER_PASS
 	item_select.select(0)
 
 func _on_item_select_item_selected(index: int) -> void:
 	current_tile_index = index
 
 func _unhandled_input(_event: InputEvent) -> void:
+	if Input.is_action_pressed("drag", true):
+		return
 	if current_category == Categories.TILES:
 		var selected_tile = tile_map.local_to_map(get_local_mouse_position())
 		if Input.is_action_pressed("click"):
 			tile_map.set_cells_terrain_connect([selected_tile], 0, current_tile_index)
 		if Input.is_action_pressed("alt_click"):
 			tile_map.set_cells_terrain_connect([selected_tile], 0, -1)
-	elif current_category == Categories.ENTITIES:
+	elif current_category == Categories.ENTITIES and not item_select.is_item_disabled(current_tile_index):
 		if Input.is_action_pressed("click"):
 			var scene = entity_placeholder_scene.instantiate()
 			scene.position = get_local_mouse_position() + Vector2(-20, -20)
@@ -63,10 +88,20 @@ func _unhandled_input(_event: InputEvent) -> void:
 			scene.set_icon()
 			created_scene.add_child(scene, true)
 			scene.set_owner(created_scene)
+			if(entities[current_tile_index].name == "Player"):
+				item_select.set_item_disabled(current_tile_index, true)
+				is_player_placed = true
 	if Input.is_action_pressed("pepper_power"):
 		should_flip = !should_flip
 		
-func _process(delta: float) -> void:
+var move_delta_start = Vector3.ZERO
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("drag", true):
+		move_delta_start = get_local_mouse_position()
+	elif Input.is_action_pressed("drag", true):
+		var move_delta = get_local_mouse_position() - move_delta_start
+		camera.global_position = camera.global_position - move_delta * delta * MOVE_SPEED * 0.004
+	
 	if Input.is_action_pressed("move_down"):
 		camera.global_position.y += MOVE_SPEED * delta  
 	if Input.is_action_pressed("move_left"):
@@ -76,8 +111,17 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("move_up"):
 		camera.global_position.y -= MOVE_SPEED * delta 
 
-
 func _on_save_button_pressed() -> void:
 	var packed_scene = PackedScene.new()
 	packed_scene.pack(created_scene)
 	ResourceSaver.save(packed_scene, "res://Scenes/LevelEditor/saved_level.tscn")
+
+func _on_test_button_pressed() -> void:
+	var packed_scene = PackedScene.new()
+	packed_scene.pack(created_scene)
+	ResourceSaver.save(packed_scene, "res://Scenes/LevelEditor/temp.tscn")
+	get_tree().change_scene_to_file("res://Scenes/LevelEditor/temp.tscn")
+
+
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Menu/main_menu.tscn")
