d99380cbfa372bc22b8e3e7b55fdc6fa09844ac1
SomeGoose106 <skylarlaec@gmail.com>
2026-07-13 10:20:32 -0400

    Powerwheel is now dynamically scalable in the editor by changing the scale of SelectPowerBg (#78)
    Signed-off-by: SomeGoose106 
    Co-authored-by: SomeGoose106

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -51,6 +51,8 @@ func _ready():
 	companion.power_changed.connect(_on_power_changed)
 	_on_power_changed(companion.get_current_power())
 	current_checkpoint_pos = global_position
+	
+	select_power.scale = select_power_sprite.scale
 	select_power.position = get_window().size/2
 	select_power_sprite.position = get_window().size/2
 
