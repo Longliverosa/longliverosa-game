966883f58aedcd73a16e0e9b2ad05dc5c08740f6
Slylecias <Slylecias@gmail.com>
2026-07-13 01:12:01 -0600

    Updated beach level with new map and visuals (#77)
    Also changed the pepper wheel to now always be on the player with a CanvasLayer, added a moving platform that goes vertically and added new tileset

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -13,8 +13,8 @@ extends CharacterBody2D
 
 @onready var label: Label = $Canvas/Label
 @onready var shield_slider: HSlider = $Canvas/ShieldSlider
-@onready var select_power: Node = $SelectPower
-@onready var select_power_sprite: Node = $"SelectPowerBg"
+@onready var select_power: Node = $CanvasLayer/SelectPower
+@onready var select_power_sprite: Node = $"CanvasLayer/SelectPowerBg"
 @onready var coyote_timer: Timer = $Timers/CoyoteTime
 @onready var jump_buffer: Timer = $Timers/JumpBuffer
 @onready var shield_cooldown : Timer = $Timers/ShieldCooldown
@@ -51,6 +51,8 @@ func _ready():
 	companion.power_changed.connect(_on_power_changed)
 	_on_power_changed(companion.get_current_power())
 	current_checkpoint_pos = global_position
+	select_power.position = get_window().size/2
+	select_power_sprite.position = get_window().size/2
 
 func _physics_process(delta):
 	if select_power.visible or controlling:
